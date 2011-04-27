a55029b5495700298c7c80c9b31cf77591972160
Christian Vuerings <vueringschristian@gmail.com>
2011-04-27 13:12:12 +0200

    Fix the keyboard events when an input text element has focus

--- mflow_keyboard_shortcuts.user.js
+++ mflow_keyboard_shortcuts.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           mflow keyboard shortcuts
-// @version        0.1
-// @namespace      http://denbuzze.wordpress.com/
+// @version        0.2
+// @namespace      http://denbuzze.com/
 // @description    Add keyboard shortcuts for the mflow music service
 // @match          http://beta.mflow.com/*
 // @include        http://beta.mflow.com/*
@@ -13,5 +13,5 @@
  * We need to do it this way since we aren't able to get to the global JavaScript variables in Chrome.
  */
 var s = document.createElement("script");
-s.innerHTML = 'var keyhandler = function(e) { var htmlPlayer = Mflow.Media.Html5MediaPlayer; if(e.keyCode == 78 || e.keyCode == 39){ Mflow.Media.PlaybackManager.nextTrack(); } else if (e.keyCode == 80) { if(htmlPlayer.isPlaying()) {  htmlPlayer.pause(); } else { htmlPlayer.resume(); } }; } ; window.addEventListener("keydown", keyhandler, false);';
+s.innerHTML = 'var keyhandler = function(e) { if(document.activeElement && document.activeElement.type == "text") {return; } var htmlPlayer = Mflow.Media.Html5MediaPlayer; if(e.keyCode == 78 || e.keyCode == 39){ Mflow.Media.PlaybackManager.nextTrack(); } else if (e.keyCode == 80) { if(htmlPlayer.isPlaying()) {  htmlPlayer.pause(); } else { htmlPlayer.resume(); } }; } ; window.addEventListener("keydown", keyhandler, false);';
 document.body.appendChild(s);
